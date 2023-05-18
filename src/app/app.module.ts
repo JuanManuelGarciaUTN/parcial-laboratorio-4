1c73f0207536e68cf5f2d8ec372c146a22538dc7
JuanManuelGarciaUTN <artlixUTN@gmail.com>
2023-05-18 17:55:34 -0300

    Creacion de projecto en nuevo

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'parcial-labo-4-8ebb1',
+    appId: '1:661336165609:web:8df2a9d46198250363b478',
+    storageBucket: 'parcial-labo-4-8ebb1.appspot.com',
+    apiKey: 'AIzaSyCM8VJMeHvcvYdSH7BEucWAHHLT56AqFEI',
+    authDomain: 'parcial-labo-4-8ebb1.firebaseapp.com',
+    messagingSenderId: '661336165609',
+  },};
